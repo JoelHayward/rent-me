770f7c40d00b1883bed7b6fa914d5fddfdeccb39
JoelHayward <joel.m.hayward@gmail.com>
2022-09-14 17:42:19 +1200

    added front, back and database to containers

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,76 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-import connectDB from './server/db.js'
+// const cors = require("cors");
+// const asyncHandler = require('express-async-handler')
+// const router = express.Router()
 
-connectDB()
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+  try {
+    //database Name
+    const databaseName = "Rentals";
+    const con = await mongoose.connect(
+      `mongodb://host.docker.internal:27017/${databaseName}`,
+      {useNewUrlParser: true}
+    );
+    console.log(`Database connected : ${con.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
+connectDB();
 
+const Schema = mongoose.Schema;
+const PropertiesSchema = new Schema({
+  id: String,
+  name: String,
+  location: String,
+  suburb: String,
+  property_type: String,
+  rooms: String,
+  bathrooms: String,
+  rent: String,
+});
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+const properties = mongoose.model("properties", PropertiesSchema);
+
+const data = {
+  id: "",
+  name: "House Four",
+  location: "Auckland",
+  suburb: "Birkenhead",
+  property_type: "house",
+  rooms: "3",
+  bathrooms: "3",
+  rent: "570",
+};
+
+const newProperties = new properties(data);
+
+// newProperties.save((error) => {
+//   if (error) {
+//     console.log("Oooops, something happened");
+//   } else {
+//     console.log("Data has been saved");
+//   }
+// });
+
+app.get('/api', (req,res) => {
+  properties.find({ })
+  .then((data) => {
+console.log('Data:', data)
+res.json(data)
+  })
+  .catch((error) => {
+    console.log('error: ', daerrorta)
+  })
+  
+  
 })
+
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
