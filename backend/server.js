c1c3c1c3c872494620dcf915a3802367e921203b
JoelHayward <joel.m.hayward@gmail.com>
2022-09-15 14:00:31 +1200

    add new pages and connected with react router dom

--- backend/server.js
+++ backend/server.js
@@ -1,73 +1,31 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
-// const asyncHandler = require('express-async-handler')
-// const router = express.Router()
-
+const cors = require("cors");
+const router = express.Router();
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    const con = await mongoose.connect(
-      `mongodb+srv://joel:mongo7zc3e@cluster0.vaoiamm.mongodb.net/?retryWrites=true&w=majority`,
-      {useNewUrlParser: true}
-    );
-    console.log(`Database connected : ${con.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-};
+app.use(cors());
 
-connectDB();
+mongoose
+  .connect(
+    "mongodb+srv://joel:mongo7zc3e@cluster0.vaoiamm.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log("Database not Connected");
+  });
 
-const Schema = mongoose.Schema;
-const PropertiesSchema = new Schema({
-  id: String,
-  name: String,
-  location: String,
-  suburb: String,
-  property_type: String,
-  rooms: String,
-  bathrooms: String,
-  rent: String,
+router.route("/Rentals").get((req, res) => {
+  listingAndReviews.find().then((foundListing) => res.json(foundListing));
 });
-
-const properties = mongoose.model("properties", PropertiesSchema);
-
-const data = {
-  id: "",
-  name: "House Four",
-  location: "Auckland",
-  suburb: "Birkenhead",
-  property_type: "house",
-  rooms: "3",
-  bathrooms: "3",
-  rent: "570",
-};
-
-const newProperties = new properties(data);
-
-// newProperties.save((error) => {
-//   if (error) {
-//     console.log("Oooops, something happened");
-//   } else {
-//     console.log("Data has been saved");
-//   }
-// });
-
-app.get('/api', (req,res) => {
-  properties.find({ })
-  .then((data) => {
-console.log('Data:', data)
-res.json(data)
-  })
-  .catch((error) => {
-    console.log('error: ', daerrorta)
-  })
-  
-  
-})
 
 app.listen(8080, function () {
   console.log("listening on 8080");
