bf45418dfd2c88d2f5bfa09a6c1e0b33119b9e46
Ayman Milhem <aymanmilhem@gmail.com>
2022-09-22 13:13:38 +1200

    having a go at quickSort

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import classes from './NavBar.module.css';
+import classes from "./NavBar.module.css";
 
 function NavBar(props) {
   return (
     <div className={classes.navBar}>
       <div className={classes.logo}>
-              <Link to="/">
-                  <img alt="logo" src="./images/Logo.png"></img>
+        <Link to="/">
+          <img alt="logo" src="./images/Logo.png"></img>
         </Link>
       </div>
       <div className={classes.navigationMenu}>{props.children}</div>
@@ -15,4 +15,4 @@ function NavBar(props) {
   );
 }
 
-export default NavBar
+export default NavBar;
--- frontend/src/pages/Rentals.js
+++ frontend/src/pages/Rentals.js
@@ -27,6 +27,79 @@ export default function Rentals() {
   // console.log(results.length);
   // console.log(results[0].name);
 
+  const numberArray = [4, 7, 1, 8, 90, 5, 30, 10, 3];
+  const stringArray = [
+    "auckland",
+    "christchurch",
+    "wellington",
+    "amman",
+    "dubai",
+    "mombasa",
+    "sydney",
+    "melbourne",
+    "ajman",
+    "barcelona",
+    "cape town",
+    "alfama",
+    "las vegas",
+    "cape verde",
+    "taipei",
+    "algorve",
+    "lisboa",
+  ];
+  const objectArray = [
+    {
+      id: 1,
+      country: "New Zealand",
+      capital: "wellington",
+    },
+    {
+      id: 2,
+      country: "Australia",
+      capital: "sydney",
+    },
+    {
+      id: 3,
+      country: "Spain",
+      capital: "Madrid",
+    },
+    {
+      id: 4,
+      country: "Italy",
+      capital: "Rome",
+    },
+    {
+      id: 5,
+      country: "France",
+      capital: "Paris",
+    },
+    {
+      id: 6,
+      country: "South Africa",
+      capital: "Cape Town",
+    },
+    {
+      id: 7,
+      country: "Taiwan",
+      capital: "Taipei",
+    },
+    {
+      id: 8,
+      country: "Egypt",
+      capital: "Cairo",
+    },
+    {
+      id: 9,
+      country: "Algeria",
+      capital: "Algiers",
+    },
+    {
+      id: 10,
+      country: "Jordan",
+      capital: "amman",
+    },
+  ];
+
   // Use Quick Sort on the fetched properties array
   // const quickSort = (array) => {
   //   if (array.length === 1) {
@@ -53,7 +126,65 @@ export default function Rentals() {
   //   }
   // };
 
+  // Customised to sort the objectArray on country - working!
+  // const quickSort = (array) => {
+  //   if (array.length === 1) {
+  //     return array;
+  //   }
+  //   const pivot = array[array.length - 1];
+  //   const pivotValue = pivot.country;
+  //   const leftArr = [];
+  //   const rightArr = [];
+
+  //   for (let i = 0; i < array.length - 1; i++) {
+  //     const arrayValue = array[i].country;
+  //     if (arrayValue < pivotValue) {
+  //       leftArr.push(array[i]);
+  //     } else {
+  //       rightArr.push(array[i]);
+  //     }
+  //   }
+
+  //   if (leftArr.length > 0 && rightArr.length > 0) {
+  //     return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+  //   } else if (leftArr.length > 0) {
+  //     return [...quickSort(leftArr), pivot];
+  //   } else {
+  //     return [pivot, ...quickSort(rightArr)];
+  //   }
+  // };
+
+  const quickSort = (array) => {
+    if (array.length === 1) {
+      return array;
+    }
+    const pivot = array[array.length - 1];
+    const pivotValue = pivot.name;
+    const leftArr = [];
+    const rightArr = [];
+
+    for (let i = 0; i < array.length - 1; i++) {
+      const arrayValue = array[i].name;
+      if (arrayValue < pivotValue) {
+        leftArr.push(array[i]);
+      } else {
+        rightArr.push(array[i]);
+      }
+    }
+
+    if (leftArr.length > 0 && rightArr.length > 0) {
+      return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+    } else if (leftArr.length > 0) {
+      return [...quickSort(leftArr), pivot];
+    } else {
+      return [pivot, ...quickSort(rightArr)];
+    }
+  };
+
   // console.log(quickSort(results));
+  // console.log(quickSort(numberArray));
+  // console.log(quickSort(stringArray));
+  // console.log(quickSort(objectArray));
 
   return (
     <div className={classes.rentals}>
